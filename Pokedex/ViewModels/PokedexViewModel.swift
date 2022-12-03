f798ac9e45bf6e1cfa446fa94b18f259dce55bbf
Daniel <danielbasaldua03@gmail.com>
2022-12-02 23:41:47 -0700

    Added the view-model that will be used for the entire app. Added functionality to connect to local database.

---
+++ Pokedex/ViewModels/PokedexViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  PokedexViewModel.swift
+//  Pokedex
+//
+//  Created by Daniel Basaldua on 12/2/22.
+//
+
+import Foundation
+import PostgresNIO
+import NIOPosix
+import Logging
+
+
+class PokedexViewModel: ObservableObject {
+    var connection: PostgresConnection?
+    let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    let logger = Logger(label: "postgres-logger")
+    let config = PostgresConnection.Configuration(
+       connection: .init(
+         host: "localhost",
+         port: 5432
+       ),
+       authentication: .init(
+         username: "danielbasaldua",
+         database: "pokemon",
+         password: "my_password"
+       ),
+       tls: .disable
+    )
+    
+    func establishConnection() async -> Bool {
+        do {
+            self.connection = try await PostgresConnection.connect(
+                on: eventLoopGroup.next(),
+                configuration: config,
+                id: 1,
+                logger: logger
+            )
+            print("Connection to pokemon database established.")
+            return true
+        } catch {
+            print("Connection to the pokemon database could not be established.")
+            return false
+        }
+    }
+}
